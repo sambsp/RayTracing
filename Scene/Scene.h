f0293d9136b0c238e0f327adf8fa28be7b8c6a98
joeshro <910833080@qq.com>
2021-05-05 22:23:02 +0800

    准备从scene开始构造

--- Scene/Scene.h
+++ Scene/Scene.h
@@ -4,9 +4,19 @@
 #ifndef RAYTRACING_SCENE_H
 #define RAYTRACING_SCENE_H
 
+#include <map>
+#include "Actor.h"
+
 
 class Scene {
+public:
+    ~Scene() {}
 
+    void AddActor(const Actor* actor);
+    void RemoveActor(const Actor* actor);
+
+private:
+    std::map<std::string, std::shared_ptr<Actor>> ActorMap;
 };
 
 
